2afd2ccd904486bad3f74f38627f295baf632a4d
NITISH PARKASH <158040914+nitishgit01@users.noreply.github.com>
2026-03-14 06:12:56 +0000

    Update homepage URL and user information in package.json and App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,14 +24,14 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
+  name: "Playfull Rebel",
   title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  email: "rebel@example.com",
+  gitHub: "playfullrebel",
+  instagram: "playfullrebel",
+  linkedIn: "playfullrebel",
   medium: "",
-  twitter: "microsoft",
+  twitter: "playfullrebel",
   youTube: "Code",
 };
 
